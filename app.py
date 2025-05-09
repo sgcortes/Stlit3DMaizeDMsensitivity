5eb676d64b9edc5fd4e9e3bec7d5f80e2b5cf56a
Silverio Garcia-Cortes <sgcortes@uniovi.es>
2025-05-09 14:14:55 +0200

    Update app.py
    Eliminacion de la seleccion del eje vertical

--- app.py
+++ app.py
@@ -1,43 +1,4 @@
 # archivo: app.py
-'''
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-# Leer datos desde archivo (o usa synthetic_df directamente si lo cargas antes)
-#df = pd.read_excel("Synthetic_Maize_Predictions.xlsx")
-
-# Leer nuevamente el DataFrame desde el archivo CSV
-synthetic_df_csv = pd.read_csv("Synthetic_Maize_Predictions.csv",encoding="latin1")
-df = synthetic_df_csv
-# Asegurar tipo correcto
-df["Condition"] = df["Condition"].astype(int)
-
-# Mapeo de colores y etiquetas
-color_labels = {1: "Favorable conds", -1: "Unfavorable conds", 0: "Intermediate conds"}
-df["Condition_Label"] = df["Condition"].map(color_labels)
-
-# Crear gráfico 3D interactivo
-fig = px.scatter_3d(
-    df,
-    x="GrowingSeason(day)",
-    y="Radiation(Mj/m2day)",
-    z="Predicted_kgDM/ha",
-    color="Condition_Label",
-    color_discrete_map={
-        "Favorable conds": "green",
-        "Unfavorable conds": "red",
-        "Intermediate conds": "gray"
-    },
-    title="3D Scatter: Growing Season vs Radiation vs Predicted kgDM/ha",
-    opacity=0.8
-)
-
-# Mostrar en Streamlit
-st.title("Dry Matter Forage Maize yield  - 3D Interactive Analysis")
-st.plotly_chart(fig)
-'''
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -58,12 +19,12 @@ st.title("Dry Matter Forage Maize Yield - 3D Interactive Analysis")
 
 # Selector para tamaño de punto
 point_size = st.slider("Select marker size", min_value=1, max_value=10, value=3)
-
+'''
 # Selector para el eje Z
 z_axis = st.selectbox("Select Z-axis variable", options=[
     "Predicted_kgDM/ha", "GrowingSeason(day)", "Radiation(Mj/m2day)"
 ], index=0)
-
+'''
 # Crear gráfico interactivo con opciones seleccionadas
 fig = px.scatter_3d(
     df,
